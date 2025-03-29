6472952c041c9eb59826fb77477e89cfc37d684f
Guilherme Pinho <guilhermepinho04@gmail.com>
2025-03-29 15:26:47 +0000

    mais alterações

--- backend/scripts/api.py
+++ backend/scripts/api.py
@@ -1,4 +1,18 @@
-for i in range(20):
-    print(i)
+import singlestoredb as s2
 
-#boas
+with s2.connect(
+    host="svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com",
+    port=3333,
+    user="guilherme", 
+    password="R9oTOoFIvdAqFg0Yox5jqd4PzF4hQ7rP",
+    database="db_smartnurse"
+) as conn:
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM fake_patients_en ORDER BY id ASC LIMIT 5;")
+        resultados = cur.fetchone()
+        print(resultados)
+        
+    # for paciente in resultados:
+    #     print(paciente)
+    #     print("-" * 50 + "\n")
